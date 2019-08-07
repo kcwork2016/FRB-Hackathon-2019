fe7e3c521e9b400e2caf658c915d76a2e5503e03
Kevin Chen <kcwork2016@outlook.com>
2019-08-06 21:45:01 -0500

    fix mapping issues

--- money-circulate-app/src/components/StoryCard.js
+++ money-circulate-app/src/components/StoryCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
@@ -7,17 +8,31 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
-export default function StoryCard(props) {
-    const { cardImage, cardText, handleButtonOnClick, cardTitle} = props;
+const styles = theme => ({
+    layout: {
+        width: 'auto',
+        marginLeft: theme.spacing.unit * 3,
+        marginRight: theme.spacing.unit * 3,
+        [theme.breakpoints.up(1100 + theme.spacing.unit * 3 * 2)]: {
+            width: 1100,
+            marginLeft: 'auto',
+            marginRight: 'auto',
+        },
+    }
+});
+
+function StoryCard(props) {
+    const { cardImage, cardText, handleButtonOnClick, cardTitle, classes} = props;
 
     return (
-        <div>
+        <div className={classes.layout}>
             <Card>
                 <CardActionArea>
                     <CardMedia
                         component="img"
                         alt="Contemplative Reptile"
                         height="280"
+                        width="100"
                         image = { cardImage }
                         title= { cardTitle }
                     />
@@ -41,8 +56,11 @@ export default function StoryCard(props) {
                         Learn More
                     </Button>
                 </CardActions>
-            </Card>
+            </Card
+       >
             <div><br/></div>
         </div>
     );
 }
+
+export default withStyles(styles)(StoryCard);
